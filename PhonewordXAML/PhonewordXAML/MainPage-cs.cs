38f038376a4403d73825cbc92f560b8dfb30a9c2
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-06-02 14:56:51 -0300

    Add project files.

---
+++ PhonewordXAML/PhonewordXAML/MainPage-cs.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Phoneword
+{
+    public class MainPage : ContentPage
+    {
+        //Create elements
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+        string translatedNumber;
+        public MainPage()
+        {
+            //Instantiate UI
+            this.Padding = new Thickness(20);
+
+            
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15
+            };
+
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a Phoneword please:",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-999-Testingicle"
+            });
+
+            panel.Children.Add(translateButton = new Button
+            {
+                Text = "Translate"
+            });
+
+            panel.Children.Add(callButton = new Button
+            {
+                Text = "Call",
+                IsEnabled = false
+                
+            });
+
+            
+            //Instantiate Listeners
+            translateButton.Clicked += OnTranslate;
+            callButton.Clicked += OnCall;
+            this.Content = panel;
+        }
+
+        //Logic for TranslateButton
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumberText.Text;
+            translatedNumber = Phoneword.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+        //Logic for callButton
+        private async void OnCall (object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert (
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    PhoneDialer.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Num not valid", "Ok");
+                }
+                catch (FeatureNotEnabledException)
+                {
+                    await DisplayAlert("Unable", "Not supported", "Ok");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Unable", "Failed", "OK");
+                }
+            }
+        }
+    }
+}
---
+++ PhonewordXAML/PhonewordXAML/PhonewordTranslator-cs.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phoneword
+{
+    public static class PhonewordTranslator
+    {
+        //Method to translate number
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return null;
+
+            raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+
+            foreach (var c in raw)
+            {
+                //Already a num? Send it through
+                if (" -0123456789".Contains(c))
+                    newNumber.Append(c);
+
+                //Char to number
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+
+                    else
+                        return null;
+                }
+            }
+            return newNumber.ToString();
+        }
+
+        //How does this inherit from parent "Contains"?
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+        static readonly string[] digits =
+        {
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+        };
+
+        //goes through digits[] cell by cell to confirm which index it belongs to.
+        static int? TranslateToNumber(char c)
+        {
+            for (int i = 0; i<digits.Length; i++)
+            {
+                if (digits[i].Contains(c))
+                    return 2 + i;
+            }
+            return null;
+        }
+    }
+}
