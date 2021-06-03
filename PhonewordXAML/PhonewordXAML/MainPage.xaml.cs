95c2064bbb4f3c534b6c32ba2fc39fe71c1f65dd
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-06-03 15:32:04 -0300

    XAML UI, no logic

--- PhonewordXAML/PhonewordXAML/MainPage.xaml.cs
+++ PhonewordXAML/PhonewordXAML/MainPage.xaml.cs
@@ -5,14 +5,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace PhonewordXAML
 {
     public partial class MainPage : ContentPage
     {
+        string translatedNumber;
         public MainPage()
         {
             InitializeComponent();
+        }
+        //Logic for TranslateButton
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = "";//phoneNumberText.Text;
+            translatedNumber = Phoneword.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        //Logic for callButton
+        private async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    PhoneDialer.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Num not valid", "Ok");
+                }
+                catch (FeatureNotEnabledException)
+                {
+                    await DisplayAlert("Unable", "Not supported", "Ok");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Unable", "Failed", "OK");
+                }
+            }
         }
     }
 }
